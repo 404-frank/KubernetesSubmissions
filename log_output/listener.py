22c0ba43c5e3bcb339283766a5aedc5a26152d8c
Frank Prins <frank@frankprins.nl>
2026-08-21 21:18:00 +0200

    implemented extra service in log_output to host a webpage

---
+++ log_output/listener.py
@@ -0,0 +1,24 @@
+import socket
+import os
+import globals
+
+HOST = ""  # Standard loopback interface address (localhost)
+PORT = 5000  # Port to listen on (non-privileged ports are > 1023)
+
+def build_response(status_code, body, content_type='text/html'):
+    # Build the HTTP response
+    status_line = f"HTTP/1.1 {status_code}\r\n"
+    headers = f"Content-Type: {content_type}\r\nContent-Length: {len(body)}\r\n"
+    response = f"{status_line}{headers}\r\n{body}"
+    return response.encode()
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen(5)
+    print(f"server is listening on port {PORT}" )
+    while True:
+       conn, addr = s.accept()
+       print(f"got a connection from {addr}")
+       output_string = "current log string: <br /><br />" + globals.get_random_string()
+       conn.sendall(build_response(200, output_string))
+       conn.close()
